dfb906cf12618755acd58cb22efbcc8b0555f82a
Lovepreet <toorlo@sheridancollege.ca>
2026-03-13 21:16:52 -0400

    First Commit

---
+++ src/main/java/test/projectlovepreet/ProjectLovepreet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.projectlovepreet;
+
+/**
+ *
+ * @author Lp_To
+ */
+public class ProjectLovepreet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
